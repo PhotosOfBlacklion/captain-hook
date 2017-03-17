1677fd590faa0e9bef796b22d66bb2891e0d96fb
Mike Griffin <mgriffin@github.com>
2017-03-17 23:19:39 +0000

    Removes a lot from the out script

--- out.rb
+++ out.rb
@@ -6,31 +6,4 @@ require 'json'
 require File.expand_path('../lib/hook', __FILE__)
 require 'rugged'
 
-@logger = Logger.new('captain-hook.log', 'daily')
-@logger.level = Logger::INFO
-@logger.datetime_format = '%Y-%m-%d %H:%M:%S'
-hook = Hook.new
-hook.are_we_running?
-
-@logger.info('In the forked process')
-photo_paths = hook.get_photo_paths
-
-if photo_paths.empty?
-  @logger.info('Nothing to process, exiting')
-  exit
-end
-
-photo_paths.each do |photo|
-  @logger.info("#{photo}")
-  hook.extract_parts(photo)
-
-  hook.download(photo)
-  hook.delete(photo)
-  hook.create_thumbnail(photo)
-  hook.copy_to_s3(photo)
-  hook.delete_temp_files(photo)
-  hook.add_photo
-end
-
-hook.create_jekyll_page
-hook.commit
+Hook.run!
