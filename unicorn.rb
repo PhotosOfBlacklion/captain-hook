806a8ead902eb10b01fc0cc13d4c15fcf8f04a3f
Mike Griffin <mgriffin@github.com>
2017-03-19 01:05:05 +0000

    Changes the unicorn socket name

--- unicorn.rb
+++ unicorn.rb
@@ -14,7 +14,7 @@ stdout_path "/home/mike/pob/captain-hook/logs/unicorn.log"
 
 # Unicorn socket
 # listen "/tmp/unicorn.[app name].sock"
-listen "/tmp/unicorn.myapp.sock"
+listen "/tmp/unicorn.pob.sock"
 
 # Number of processes
 # worker_processes 4
