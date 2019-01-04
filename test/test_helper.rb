5a7790fbe515aa771ebfa2d03f258a7af224aa4a
Mike Griffin <mgriffin@github.com>
2019-01-04 12:40:51 +0000

    Add the coverage commands

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,12 @@
+if ENV['COVERAGE']
+  require "simplecov"
+
+  SimpleCov.start do
+    add_filter 'test'
+    add_filter 'vendor'
+  end
+end
+
 require "minitest/autorun"
 require "./lib/album"
 require "./lib/hook"
