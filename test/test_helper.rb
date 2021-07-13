d2a73a43e70ed669689ba453b93ee46fd70d9701
Mike Griffin <mgriffin@github.com>
2021-07-13 18:29:46 +0100

    Adds simplecov for code coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 require "minitest/autorun"
 require "sequel"
+require "simplecov"
+
+SimpleCov.start do
+  add_filter '/vendor/'
+  add_filter '/test/'
+end
 
 DB = Sequel.connect('sqlite://db/hook_test.db')
 
